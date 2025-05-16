eb33639ad13b920851d40f5990d1fa7bf11c9d57
RobertoFloris <florisroberto99@gmail.com>
2025-05-16 20:50:29 +0200

    [add] filter

--- app/Http/Controllers/Api/MangaController.php
+++ app/Http/Controllers/Api/MangaController.php
@@ -8,17 +8,47 @@ use Illuminate\Http\Request;
 
 class MangaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $mangas = Manga::with('type')->get();
+        $query = Manga::with('type');
 
-        return response()->json(
-            [
-                "success" => "true",
-                "data" => $mangas
-            ]
-        );
+        if ($request->filled('title')) {
+            $query->where('title', 'like', '%' . $request->title . '%');
+        }
+
+        if ($request->filled('type_id')) {
+            $query->where('type_id', $request->type_id);
+        }
+
+        if ($request->filled('order')) {
+            switch ($request->order) {
+                case 'alphabetical':
+                    $query->orderBy('title', 'asc');
+                    break;
+                case 'newest':
+                    $query->orderBy('year_of_publication', 'desc');
+                    break;
+                case 'oldest':
+                    $query->orderBy('year_of_publication', 'asc');
+                    break;
+                case 'latest_added':
+                    $query->orderBy('id', 'asc');
+                    break;
+            }
+        } else {
+            $query->orderBy('title', 'asc');
+        }
+
+        $mangas = $query->get();
+
+        return response()->json([
+            "success" => "true",
+            "data" => $mangas
+        ]);
     }
+
+
+
 
     public function show(Manga $manga)
     {
